7bdb9a0e1cc0a65aeae2c072f3b76118fc0bd452
= <=>
2022-11-18 13:47:02 +0000

    libft_0

--- CommonCore/0_libft/ft_strlcat.c
+++ CommonCore/0_libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: falves-b <falves-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 00:04:28 by falves-b          #+#    #+#             */
-/*   Updated: 2022/11/09 17:31:34 by falves-b         ###   ########.fr       */
+/*   Updated: 2022/11/18 12:24:56 by falves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,23 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int		dst_len;
+	size_t	dst_len;
+	size_t	src_len;
 	size_t	i;
 
 	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	i = 0;
-	while (dst[i] && i < size)
-		i++;
-	while (src[i - dst_len] && i < size - 1)
+	if (size < dst_len + 1)
+		return (size + src_len);
+	if (size > dst_len + 1)
 	{
-		dst[i] = src[i - dst_len];
-		i++;
+		while (src[i] != '\0' && dst_len + 1 + i < size)
+		{
+			dst[dst_len + i] = src[i];
+			i++;
+		}
 	}
-	dst[i] = '\0';
-	return (i);
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
-
-/* size_t ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	offset;
-	int		d_len;
-	int		s_len;
-	int		src_index;
-
-    d_len = ft_strlen(dst);
-    s_len = ft_strlen(src);
-
-    offset = d_len;
-
-    src_index = 0;
-    while( *(src+src_index) != '\0' )
-    {
-        *(dst+offset) = *(src+src_index);
-        offset++;
-        src_index++;
-        if( offset==size-1)
-            break;
-    }
-    *(dst+offset) = '\0';
-
-    return( d_len+s_len );
-} */
