ff0d6fdcb31275929b1964c2adcab9194384f00d
= <=>
2022-11-22 01:14:26 +0000

    libft_0

--- CommonCore/0_libft/ft_split.c
+++ CommonCore/0_libft/ft_split.c
@@ -6,72 +6,47 @@
 /*   By: falves-b <falves-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:09:40 by falves-b          #+#    #+#             */
-/*   Updated: 2022/11/21 00:37:47 by falves-b         ###   ########.fr       */
+/*   Updated: 2022/11/22 01:06:31 by falves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include "libft.h"
- 
-unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
-{
-	unsigned int	i;
-	int				strlen;
+#include "libft.h"
 
-	strlen = 0;
-	while (src[strlen] != '\0')
-		strlen++;
-	i = 0;
-	if (size == 0)
-		return (strlen);
-	while (i < size - 1 && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (strlen);
+static char	*ft_strchrnul(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = ft_strchr(s, c);
+	if (!ptr)
+		ptr = ft_strchr(s, '\0');
+	return (ptr);
 }
 
-
-char	**ft_split(char const *s, char c) //use memchr maybe
+char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	char	**result_start;
 	char	*ptr;
 
-	result = malloc(strlen(s) + 1);
+	if (!s)
+		return (NULL);
+	result = ft_calloc(ft_strlen(s) + 1000, 1);
 	result_start = result;
-	while (*s)
+	if (!result || !ft_strlen(s))
+		return (result_start);
+	while (1)
 	{
-		ptr = NULL;
 		if (*s != c)
 		{
-			ptr = strchr(s, c);
-			if (!ptr)
-				ptr = strchr(s, '\0');
-			*result = malloc(ptr - s + 1);
-			ft_strlcpy(*result, s, ptr - s + 1);
-			s = ptr + 1;
-			result++;
+			if (*s == '\0')
+				break ;
+			ptr = ft_strchrnul(s, c);
+			*result = ft_calloc(ptr - s + 1, 1);
+			ft_strlcpy(*result++, s, ptr - s + 1);
+			s = ptr;
 		}
 		else
 			s++;
 	}
 	return (result_start);
 }
-
-int main()
-{
-	char *str = " 1 2";
-	char **split = ft_split(str, ' ');	
-	while (1)
-	{
-		if (*split == NULL)
-		{
-			printf("null %p\n", *split);
-			break ;
-		}
-		printf("%s\n", *split);
-		split++;
-	}
-} */
